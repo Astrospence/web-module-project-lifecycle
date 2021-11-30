65c5660f7208f597b45364df4d3d83bfb139720f
Astrospence <illustrator.wood@gmail.com>
2021-11-30 14:08:54 -0700

    initial scaffolding

---
+++ src/components/Follower.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class Follower extends React.Component {
+    constructor() {
+        super();
+    }
+
+    render() {
+        return (
+            <div>
+
+            </div>
+        )
+    }
+}
+
+export default Follower;
---
+++ src/components/User.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class User extends React.Component {
+    constructor() {
+        super();
+    }
+
+    render() {
+        return(
+            <div>
+
+            </div>
+        )
+    }
+}
+
+export default User;
