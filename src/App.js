1986eb956dd65fa10f9dfe98ea9109ad01b7daef
Astrospence <illustrator.wood@gmail.com>
2021-11-30 16:22:46 -0700

    axios calls successful-it causes two rerenders which is inneficient but will address that later

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import axios from 'axios';
 import User from './components/User';
 
 class App extends React.Component {
@@ -6,11 +7,46 @@ class App extends React.Component {
     super();
     this.state = {
       user: '',
-      userInfo: {},
+      userInfo: {
+        photo: '',
+        username: 'Astrospence',
+        totalFollowers: ''
+      },
       followers: []
     }
   }
+
+  componentDidMount() {
+    axios.get(`https://api.github.com/users/${this.state.userInfo.username}`)
+    .then(resp => {
+      const newUserInfo = {
+        photo: resp.data.avatar_url,
+        username: resp.data.login,
+        totalFollowers: resp.data.followers
+      }
+      this.setState({
+        ...this.state,
+        user: resp.data.name,
+        userInfo: newUserInfo
+      })
+    })
+    .catch(err => {
+      console.log(err)
+    })
+    axios.get(`https://api.github.com/users/${this.state.userInfo.username}/followers`)
+    .then(resp => {
+      this.setState({
+        ...this.state,
+        followers: resp.data
+      })
+      })
+      .catch(err => {
+        console.log(err)
+      })
+  }
+
   render() {
+    console.log('render');
     return(<div>
       <User />
     </div>);
